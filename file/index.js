e753668ff07cffc086a5fbd66f86f9cf41c7c0b4
Ktari ayman <aymanktari31@gmail.com>
2022-11-03 23:47:22 +0100

    import + read data with csv file

---
+++ file/index.js
@@ -0,0 +1,16 @@
+const { parse } = require ("csv-parser");
+const  fs  = require ("fs");
+
+const results= []
+
+fs.createReadStream('kepler_data.csv')
+    .on('data',(data) => {
+        results.push(data);
+    })
+    .on('error' , (err) => {
+        console.log(err)
+    })
+    .on('end', ()=> {
+        console.log(results)
+        console.log('done')
+    })
