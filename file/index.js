0681c1c33cde9a2e0bd0807d6c17ae2072c0f9ef
Ktari ayman <aymanktari31@gmail.com>
2022-11-05 11:22:09 +0100

    add habitablePlanets function

--- file/index.js
+++ file/index.js
@@ -1,16 +1,29 @@
-const { parse } = require ("csv-parser");
+const  parse  = require ("csv-parser");
 const  fs  = require ("fs");
 
-const results= []
+const habitablePlanets = []
 
-fs.createReadStream('kepler_data.csv')
-    .on('data',(data) => {
-        results.push(data);
-    })
-    .on('error' , (err) => {
-        console.log(err)
-    })
-    .on('end', ()=> {
-        console.log(results)
-        console.log('done')
-    })
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'&& planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
+}
+
+  fs.createReadStream('kepler_data.csv')
+  .pipe(parse({
+    comment: '#',
+    columns: true,
+  }))
+  .on('data', (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on('error', (err) => {
+    console.log(err);
+  })
+  .on('end', () => {
+    console.log(habitablePlanets.map((planet) => {
+      return planet['kepler_name'];
+    }));
+    console.log(`${habitablePlanets.length} habitable planets found!`);
+  });
