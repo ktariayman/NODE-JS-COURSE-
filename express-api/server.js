0fe0db05bf7f9d5db09c4e2fb4c22d2de6c4b350
Ktari ayman <aymanktari31@gmail.com>
2022-11-06 12:18:21 +0100

    serving websites

--- express-api/server.js
+++ express-api/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
+const path = require('path');
 
 const app = express()
-
 const port  = 8000
 
 const friendsRouter = require("./router/friends.router")
@@ -17,6 +17,7 @@ const messagesRouter = require("./router/messages.router")
 
 // })
 
+app.use('/site', express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 
 // routes 
