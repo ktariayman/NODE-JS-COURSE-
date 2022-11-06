2f64b7185e1956ed3c9942ef53f3942804d736c0
Ktari ayman <aymanktari31@gmail.com>
2022-11-06 13:26:25 +0100

    template engine +layout separation

--- express-api/controllers/messages.controller.js
+++ express-api/controllers/messages.controller.js
@@ -1,7 +1,11 @@
 const path = require("path")
 
 const getMessages =(req,res) => {
-        res.sendFile(path.join(__dirname , "..", "messages","public","pic.jpg"))
+        // res.sendFile(path.join(__dirname , "..", "messages","public","pic.jpg"))
+        res.render('friends', {
+            title: 'Messages to my Friends!',
+            friend: 'Ktari ayman',
+          });
 
     }
 
--- express-api/server.js
+++ express-api/server.js
@@ -2,10 +2,17 @@ const express = require('express')
 const path = require('path');
 
 const app = express()
-const port  = 8000
 
 const friendsRouter = require("./router/friends.router")
 const messagesRouter = require("./router/messages.router")
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+const port  = 8000
+
+app.use('/site', express.static(path.join(__dirname, 'public')));
+app.use(express.json())
 
 
 //     // simple middleware
@@ -17,10 +24,13 @@ const messagesRouter = require("./router/messages.router")
 
 // })
 
-app.use('/site', express.static(path.join(__dirname, 'public')));
-app.use(express.json())
-
 // routes 
+app.get('/',(req,res)=> {
+    res.render('index' , {
+        title:'Expres API',
+        caption:'I believe that '
+    })
+})
 app.use('/friends',friendsRouter)
 app.use('/messages',messagesRouter)
 app.listen(port, ()=> {
