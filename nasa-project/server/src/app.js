f767a64d8a3a6e7c2d319b43991eecd3b20568f8
Ktari ayman <aymanktari31@gmail.com>
2023-01-02 21:18:30 +0100

    serve react to production

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const planetRouter = require("./routes/planets/planets.router");
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
 app.use(planetRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 module.exports = app;
